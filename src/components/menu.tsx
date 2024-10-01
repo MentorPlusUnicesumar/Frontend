41b1b2b9fefb035274c1ec01fca889f299493579
Guilherme <guilhermemen2003@gmail.com>
2024-10-01 08:50:49 -0300

    Ajustes apresentação

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,147 +1,30 @@
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import {
-  Box,
-  Button,
-  Flex,
-  Img,
-  Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-  theme,
-} from "@chakra-ui/react";
-import { useContext, useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/authContext";
-import pedro from "../imgs/pedro.jpg";
-import myTheme from "../mytheme";
+import { Box, Flex, Img, Link, Text } from "@chakra-ui/react";
+import pedro from "../imgs/pedro.jpg"
+import { useState } from "react";
+import { ItemMenu } from "./ItemMenu";
 
-export function MenuUsuario() {
-  const links = [
-    {
-      name: "Minhas mentorias",
-      qnt: 2,
-      link: "/minhas-mentorias",
-    },
-    {
-      name: "buscar mentores",
-      qnt: null,
-      link: "/buscar-mentores",
-    },
-    {
-      name: "Notificações",
-      qnt: 5,
-      link: "/notificacoes",
-    },
-    {
-      name: "Chat",
-      qnt: 1,
-      link: "/chat",
-    },
-  ];
+export function Menu() {
+    const [menuSelecionado, setMenuSelecionado] = useState("Minhas mentorias")
 
-  const { user, signOut } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [linkSelecionado, setLinkSelecionado] = useState("minhas-mentorias");
-  const nomeUsuario = user?.name.split(" ")[0];
-
-  useEffect(() => {
-    setLinkSelecionado(location.pathname);
-    console.log("linkSelecionado", linkSelecionado);
-  }, []);
-
-  function menuSelect(action: "logout" | "perfil") {
-    if (action === "logout") {
-      signOut();
-    } else if (action === "perfil") {
-      navigate("/perfil");
+    function selecionado(selected: string) {
+        setMenuSelecionado(selected)
     }
-  }
 
-  return (
-    <Flex
-      px={"150px"}
-      py={"10px"}
-      alignItems={"center"}
-      w={"full"}
-      h={"70px"}
-      bg={"linear-gradient(to right, #000024 60%, #000030 100%)"}
-      justifyContent={"space-between"}
-    >
-      <Text
-        color={"white"}
-        fontSize={"2xl"}
-        fontWeight={"bold"}
-        onClick={() => navigate("/minhas-mentorias")}
-      >
-        Mentor +
-      </Text>
+    return (
+        <Flex px={'60px'} py={'10px'} alignItems={'center'} w={'full'} h={'70px'} bg={'#1D428A'} justifyContent={'space-between'}>
+            <Text color={'white'} fontSize={'2xl'} fontWeight={'bold'}>Mentor +</Text>
 
-      <Flex justifyContent={"center"} alignItems={"center"} gap={6}>
-        {links.map((link) => {
-          return (
-            <Flex flexDir={"row"} gap={2}>
-              <Link onClick={() => navigate(`${link.link}`)}>
-                <Text
-                  textDecorationLine={
-                    link.link === linkSelecionado ? "underline" : "none"
-                  }
-                  color={"white"}
-                  fontWeight={"light"}
-                >
-                  {link.name}
-                </Text>
-              </Link>
-
-              {link.qnt ? (
-                <Box
-                  display={"flex"}
-                  justifyContent={"center"}
-                  alignItems={"center"}
-                  w={"25px"}
-                  h="20px"
-                  bg={"white"}
-                  borderRadius={"5px"}
-                >
-                  <Text color={"#1D428A"}>{link.qnt}</Text>
-                </Box>
-              ) : null}
+            <Flex justifyContent={'space-between'} w={'550px'}>
+                <ItemMenu selecionado={menuSelecionado} onClick={() => selecionado('Minhas mentorias')} name="Minhas mentorias" qntd={2} />
+                <ItemMenu selecionado={menuSelecionado} onClick={() => selecionado('Encontrar mentores')} name="Encontrar mentores" />
+                <ItemMenu selecionado={menuSelecionado} onClick={() => selecionado('Notificações')} name="Notificações" qntd={5} />
+                <ItemMenu selecionado={menuSelecionado} onClick={() => selecionado('Chat')} name="Chat" qntd={1} />
             </Flex>
-          );
-        })}
-      </Flex>
 
-      <Box
-        justifyContent={"center"}
-        alignItems={"center"}
-        display={"flex"}
-        flexDir={"column"}
-      >
-        <Img src={pedro} w={"40px"} h={"40px"} borderRadius={"20px"} />
-        <Menu>
-          <MenuButton
-            as={Button}
-            variant="link"
-            rightIcon={<ChevronDownIcon />}
-            style={{
-              textDecoration: "none",
-              padding: 0,
-              color: "white",
-              fontWeight: "300",
-            }}
-            _focus={{ boxShadow: "none" }}
-          >
-            {nomeUsuario}
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={() => menuSelect("perfil")}>Meu perfil</MenuItem>
-            <MenuItem onClick={() => menuSelect("logout")}>Sair</MenuItem>
-          </MenuList>
-        </Menu>
-      </Box>
-    </Flex>
-  );
+            <Box>
+                <Img src={pedro} w={'40px'} h={'40px'} borderRadius={'20px'} />
+                <Text color={'white'}>Pedro</Text>
+            </Box>
+        </Flex>
+    )
 }
