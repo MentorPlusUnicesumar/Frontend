2d1b64deb7c5eec60e75f4e55cbb3f01ca60de62
Guilherme Men <guilhermemen2003@gmail.com>
2024-10-15 22:06:03 -0300

    Layout do chat

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,11 +1,20 @@
 import { useContext, useEffect, useState } from 'react';
-import { Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Img, Text } from '@chakra-ui/react';
 import { AuthContext } from '../context/authContext';
+import { useChat } from '../utils/useChat';
+import { useQuery } from 'react-query';
+import { MenuUsuario } from '../components/menu';
 
 export function Chat() {
     const {socket, user} = useContext(AuthContext)
     const [messages, setMessages] = useState<string[]>([]);
-    
+    const {getChats} = useChat();
+    const { data } = useQuery({
+        queryKey: ["chats"],
+        queryFn: async () => getChats(),
+      });
+
+    console.log('chats', data)
 
     useEffect(() => {
         socket!.on('message', (message) => {
@@ -15,6 +24,8 @@ export function Chat() {
         return () => {
           socket!.off('message');
         };
+
+       
       }, []);
 
       const sendMessage = () => {
@@ -27,12 +38,25 @@ export function Chat() {
       };
 
       return (
-        <Flex w={'full'} h={'full'}>
-            {messages.map((msg, index) => (
+        <Flex w={'full'} h={'full'} flexDir={"column"}>
+              <MenuUsuario />
+
+              <Flex w={'25%'} h={'full'} bg={'#D2D2D2'}>
+                {data?.map((chat) => (
+                    <Box display={'flex'} flexDir={'column'} gap={2} alignItems={'center'} w={'100%'} h={'60px'}>                        
+                        <Flex>
+                            <Img w={'40px'} h={'40px'} borderRadius={'full'} src={user?.typeUser === 'Aluno' ? chat.idMentor.fotos : chat.idAluno.fotos}/>
+                            <Text>{user?.typeUser === 'Aluno' ? chat.idMentor.name : chat.idAluno.name}</Text>
+                        </Flex>
+                        <Box w={'80%'} h={'1px'} bg={'red'}/>
+                    </Box>
+                ))}
+              </Flex>
+            {/* {messages.map((msg, index) => (
                 <Text key={index}>{msg}</Text>
         ))}
 
-        <Button onClick={sendMessage}>Enviar mensagem</Button>
+        <Button onClick={sendMessage}>Enviar mensagem</Button> */}
         </Flex>
       )
 }
--- src/utils/useChat.ts
+++ src/utils/useChat.ts
@@ -1,13 +1,33 @@
 import api from "../api";
 
+type User = {     
+    fotos: string,
+    name: string,
+    _id: string   
+}
+
+type getChats = {
+    lastMessage: boolean,
+    idAluno: User,
+    idMentor: User,
+    _id: string,
+
+}
+
 export function useChat() {
-    async function createChat(idMentor: string, idAluno: string) {
-        const body = {idAluno, idMentor}
+    async function createChat(idAluno: string, idMentor: string) {
+        const body = {idAluno: idAluno, idMentor: idMentor}
 
         const {data} = await api.post("chat", body)
 
         return data;
     }
 
-    return {createChat};
+    async function getChats() {
+        const {data} = await api.get<getChats[]>('chat');
+
+        return data;
+    }
+
+    return {createChat, getChats};
 }
